27eef23454e26f42bd939c4eed69c94f61932a10
Dylan Thomas <kdylanthomas@gmail.com>
2016-05-20 17:25:44 -0500

    created a temporary landing page with links to contact info

---
+++ main.js
@@ -0,0 +1,3 @@
+$("[data-toggle=popover]").popover(); // activate email popover
+
+console.log("This landing page will remain live while \nI'm working on the final version of my site, \nwhich I'm currently building with Node.js. \nCome back soon and see it!")
