7a879a1fc78a9545d5f5d94354d5db149999c89e
Dylan Thomas <kdylanthomas@gmail.com>
2016-06-05 08:25:16 -0500

    added offset for scrolling from nav bar

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
+var offset = 50;
+
+//control offset for scrolling
+$('#custom-nav li a').click(function(event) {
+    event.preventDefault();
+    $($(this).attr('href'))[0].scrollIntoView({behavior: 'smooth'});
+    scrollBy(0, -offset);
+});
+
+// popover for email
 $("[data-toggle=popover]").popover(); // activate email popover
 
-console.log("This landing page will remain live while \nI'm working on the final version of my site, \nwhich I'm currently building with Node.js. \nCome back soon and see it!")
+
