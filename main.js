b4ad894808611e4af182f0300e1fa7c14c953a99
Dylan Thomas <kdylanthomas@gmail.com>
2016-06-05 21:03:40 -0500

    added contact information and footer

--- main.js
+++ main.js
@@ -1,4 +1,11 @@
-smoothScroll.init({offset: 50});
+smoothScroll.init({offset: 50}); // initialize smooth scrolling
+
+// close navbar-collapse when user clicks a link
+$('.custom-nav-links li a').on('click', function(){
+  if($('.navbar-toggle').css('display') !='none'){
+      $(".navbar-toggle").trigger( "click" );
+  }
+});
 
 // popover for email
 $("[data-toggle=popover]").popover(); // activate email popover
