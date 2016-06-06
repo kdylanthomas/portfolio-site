21ebcde4b5e5c08c86d41543d904289d09cf87ee
Dylan Thomas <kdylanthomas@gmail.com>
2016-06-05 19:46:32 -0500

    added smooth-scroll dependency to use with nav bar

--- main.js
+++ main.js
@@ -1,11 +1,4 @@
-var offset = 50;
-
-//control offset for scrolling
-$('#custom-nav li a').click(function(event) {
-    event.preventDefault();
-    $($(this).attr('href'))[0].scrollIntoView({behavior: 'smooth'});
-    scrollBy(0, -offset);
-});
+smoothScroll.init({offset: 50});
 
 // popover for email
 $("[data-toggle=popover]").popover(); // activate email popover
